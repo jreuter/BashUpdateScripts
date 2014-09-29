fc9255fb35db60a3ee24c6d94d614c5d5a86420a
Jarrod Reuter <jarrod.reuter@gmail.com>
2014-09-29 19:53:40 -0400

    Updating the update script to add and check for root, etc.

--- updateBash.sh
+++ updateBash.sh
@@ -1,6 +1,11 @@
-# depends on:
-# patch
-# build-essential
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${DIR}/utils/root_check.sh
+source ${DIR}/utils/log_errors.sh
+
+# Add sources for old versions of Ubuntu and install pre-reqs.
+./addSources.sh
 
 # This code came from here:
 # http://superuser.com/questions/816787/how-do-i-patch-the-shellshock-vulnerability-on-an-obsolete-ubuntu-system-that-i
@@ -15,11 +20,10 @@ tar zxvf bash-4.3.tar.gz
 cd bash-4.3
 
 # Apply all patches.
-for i in $(seq -f "%03g" 0 26);do patch -p0 < ../bash43-$i; done
+for i in $(seq -f "%03g" 0 26);do patch -p0 < ../bash43-${i}; done
 
 # Build and install.
 ./configure --prefix=/ && make && make install
 cd .. 
 cd ..
 rm -r src
-
