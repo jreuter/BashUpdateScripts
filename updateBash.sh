ad540ea4b6dfd557e17cf7d9a7e82b5b9d996241
Jarrod Reuter <jarrod.reuter@gmail.com>
2014-09-29 23:31:21 -0400

    Making variables consistent.

--- updateBash.sh
+++ updateBash.sh
@@ -5,7 +5,7 @@ source ${DIR}/utils/root_check.sh
 source ${DIR}/utils/log_errors.sh
 
 # Add sources for old versions of Ubuntu and install pre-reqs.
-./addSources.sh
+${DIR}/addSources.sh
 
 # This code came from here:
 # http://superuser.com/questions/816787/how-do-i-patch-the-shellshock-vulnerability-on-an-obsolete-ubuntu-system-that-i
@@ -15,7 +15,7 @@ cd src
 wget http://ftp.gnu.org/gnu/bash/bash-4.3.tar.gz
 
 # Download all patches.
-for i in $(seq -f "%03g" 0 26); do wget http://ftp.gnu.org/gnu/bash/bash-4.3-patches/bash43-$i; done
+for i in $(seq -f "%03g" 0 26); do wget http://ftp.gnu.org/gnu/bash/bash-4.3-patches/bash43-${i}; done
 tar zxvf bash-4.3.tar.gz 
 cd bash-4.3
 
@@ -24,6 +24,6 @@ for i in $(seq -f "%03g" 0 26);do patch -p0 < ../bash43-${i}; done
 
 # Build and install.
 ./configure --prefix=/ && make && make install
-cd .. 
-cd ..
+cd ${DIR} 
 rm -r src
+
